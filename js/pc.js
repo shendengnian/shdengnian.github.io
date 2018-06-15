0cdf769ba457aaaf88e7ba53af890c7e1609b40d
shendengnian <lovesmilegod@qq.com>
2018-06-15 10:41:57 +0800

    Site updated: 2018-06-15 10:41:57

--- js/pc.js
+++ js/pc.js
@@ -1,1 +1,218 @@
-define([],function(){var t,e=(t=$(".tips-box"),{show:function(){t.removeClass("hide")},hide:function(){t.addClass("hide")},init:function(){}}),o=function(){var t=$("#myonoffswitch"),o=$(".second-part"),i=$(".first-part");t.click(function(){t.hasClass("clicked")?(t.removeClass("clicked"),o.removeClass("turn-left"),i.removeClass("turn-left")):(t.addClass("clicked"),o.addClass("turn-left"),i.addClass("turn-left"),resetTags())});var n=!1,s=!1;$(".icon").bind("mouseenter",function(){n=!0,e.show()}).bind("mouseleave",function(){n=!1,setTimeout(function(){s||e.hide()},100)}),$(".tips-box").bind("mouseenter",function(){s=!0,e.show()}).bind("mouseleave",function(){s=!1,setTimeout(function(){n||e.hide()},100)}),$(".tips-inner li").bind("click",function(){var t,o,i=$(this).index();o=["-webkit-transform: translate(-"+100*(t=i)+"%, 0);","-moz-transform: translate(-"+100*t+"%, 0);","-o-transform: translate(-"+100*t+"%, 0);","-ms-transform: translate(-"+100*t+"%, 0);","transform: translate(-"+100*t+"%, 0);"],$(".switch-wrap")[0].style.cssText=o.join(""),$(".icon-wrap").addClass("hide"),$(".icon-wrap").eq(t).removeClass("hide"),e.hide()})};yiliaConfig.isPost&&($(".post-list").addClass("toc-article"),$("#post-nav-button > a:nth-child(2)").click(function(){$("#post-nav-button .fa-bars,#post-nav-button .fa-times").toggle(),$(".post-list").toggle(300),$(".toc").length>0?$("#toc, #tocButton").toggle(200,function(){$(".switch-area").is(":visible")&&($("#toc, .switch-btn, .switch-area").toggle(),$("#tocButton").attr("value",yiliaConfig.toc[0]))}):$(".switch-btn, .switch-area").fadeToggle(300)}));if(yiliaConfig.jquery_ui[0])require([yiliaConfig.jquery_ui[1]],function(){!function(t,e){var o=document.createElement("link");o.rel="stylesheet",o.href=t;var i=document.querySelector("head");i.insertBefore(o,i.childNodes[e])}(yiliaConfig.jquery_ui[2],25),$().tooltip&&(navigator.userAgent.match(/(iPhone|iPad|Android|ios|PlayBook|Touch)/i)||($("[title]").tooltip({show:{effect:"blind",delay:250,duration:55}}),$("#scroll").tooltip({show:{effect:"clip",delay:600,duration:50}}),$("#tocButton, #comments").tooltip({show:{delay:1200}}),$(".ds-replybox form").off("tooltip"),$("#post-nav-button").tooltip({show:{effect:"clip",delay:280,duration:65}}),$("#post-nav-button > a:nth-child(2)").tooltip({show:{delay:1500}}),$(".social").tooltip({show:{effect:"scale",delay:350,duration:70}})))});if(yiliaConfig.search)require([yiliaConfig.rootUrl+"js/search.js"],function(){var t=document.querySelector("#local-search-input"),e=$("#toc, #tocButton, .post-list, #post-nav-button a:nth-child(2)"),o=$("#search-form .fa-times"),i=$("#local-search-result"),n=function(){var t=yiliaConfig.rootUrl+"search.xml";searchFunc(t,"local-search-input","local-search-result")},s=t.getAttribute("searchonload");yiliaConfig.search&&"true"===s?n():t.onfocus=function(){n()},t.oninput=function(){e.css("visibility","hidden"),o.show()},t.onkeydown=function(){if(13==event.keyCode)return!1},resetSearch=function(){e.css("visibility","initial"),i.html(""),document.querySelector("#search-form").reset(),o.hide(),$(".no-result").hide()},i.bind("DOMNodeRemoved DOMNodeInserted",function(t){$(t.target).text()?$(".no-result").hide():$(".no-result").show(200)})});return{init:function(){resetTags(),o(),e.init()}}});
+define([], function(){
+
+    var Tips = (function(){
+
+        var $tipBox = $(".tips-box");
+
+        return {
+            show: function(){
+                $tipBox.removeClass("hide");
+            },
+            hide: function(){
+                $tipBox.addClass("hide");
+            },
+            init: function(){
+                
+            }
+        }
+    })();
+
+    var slide = function(idx){
+        // 修复IE10+切换无效的bug
+        var $wrap = $(".switch-wrap"),
+          transform = [
+              '-webkit-transform: translate(-' + idx * 100 + '%, 0);',
+              '-moz-transform: translate(-' + idx * 100 + '%, 0);',
+              '-o-transform: translate(-' + idx * 100 + '%, 0);',
+              '-ms-transform: translate(-' + idx * 100 + '%, 0);',
+              'transform: translate(-' + idx * 100 + '%, 0);'
+          ];
+        $wrap[0].style.cssText = transform.join('');
+        $(".icon-wrap").addClass("hide");
+        $(".icon-wrap").eq(idx).removeClass("hide");
+    }
+
+    var bind = function(){
+        var switchBtn = $("#myonoffswitch");
+        var tagcloud = $(".second-part");
+        var navDiv = $(".first-part");
+        switchBtn.click(function(){
+            if(switchBtn.hasClass("clicked")){
+                switchBtn.removeClass("clicked");
+                tagcloud.removeClass("turn-left");
+                navDiv.removeClass("turn-left");
+            }else{
+                switchBtn.addClass("clicked");
+                tagcloud.addClass("turn-left");
+                navDiv.addClass("turn-left");
+                resetTags();
+            }
+        });
+
+        var timeout;
+        var isEnterBtn = false;
+        var isEnterTips = false;
+
+        $(".icon").bind("mouseenter", function(){
+            isEnterBtn = true;
+            Tips.show();
+        }).bind("mouseleave", function(){
+            isEnterBtn = false;
+            setTimeout(function(){
+                if(!isEnterTips){
+                    Tips.hide();
+                }
+            }, 100);
+        });
+
+        $(".tips-box").bind("mouseenter", function(){
+            isEnterTips = true;
+            Tips.show();
+        }).bind("mouseleave", function(){
+            isEnterTips = false;
+            setTimeout(function(){
+                if(!isEnterBtn){
+                    Tips.hide();
+                }
+            }, 100);
+        });
+
+        $(".tips-inner li").bind("click", function(){
+            var idx = $(this).index();
+            slide(idx);
+            Tips.hide();
+        });
+    }
+
+    var miniArchives = function(){
+        if(yiliaConfig.isPost) {
+            $(".post-list").addClass("toc-article");
+            $("#post-nav-button > a:nth-child(2)").click(function() {
+                $("#post-nav-button .fa-bars,#post-nav-button .fa-times").toggle();
+                $(".post-list").toggle(300);
+                if ($(".toc").length > 0) {
+                    $("#toc, #tocButton").toggle(200, function() {
+                        if ($(".switch-area").is(":visible")) {
+                            $("#toc, .switch-btn, .switch-area").toggle();
+                            $("#tocButton").attr("value", yiliaConfig.toc[0]);
+                            }
+                        });
+                }
+                else {
+                    $(".switch-btn, .switch-area").fadeToggle(300);
+                }
+            });
+        }
+    }()
+
+    if (yiliaConfig.jquery_ui[0]) {
+        var tooltip = function(){
+            require([yiliaConfig.jquery_ui[1]], function(){
+                var loadCSS = function (url, num) {
+                    var link = document.createElement("link");
+                    link.rel = "stylesheet";
+                    link.href = url;
+                    var head = document.querySelector("head");
+                    head.insertBefore(link, head.childNodes[num]);
+                }
+                loadCSS(yiliaConfig.jquery_ui[2], 25);
+                if (!$().tooltip) return;
+                if (navigator.userAgent.match(/(iPhone|iPad|Android|ios|PlayBook|Touch)/i)) return;
+                $("[title]").tooltip({
+                    show: {
+                        effect: 'blind',
+                        delay: 250,
+                        duration: 55,
+                    }
+                })
+                $("#scroll").tooltip({
+                    show: {
+                        effect: 'clip',
+                        delay: 600,
+                        duration: 50,
+                    }
+                })
+                $("#tocButton, #comments").tooltip({
+                    show: {
+                        delay: 1200,
+                    }
+                })
+                $(".ds-replybox form").off("tooltip")
+                $("#post-nav-button").tooltip({
+                    show: {
+                        effect: 'clip',
+                        delay: 280,
+                        duration: 65,
+                    }
+                })
+                $("#post-nav-button > a:nth-child(2)").tooltip({
+                    show: {
+                        delay: 1500,
+                    }
+                })
+                $(".social").tooltip({
+                    show: {
+                        effect: 'scale',
+                        delay: 350,
+                        duration: 70,
+                    }
+                })
+            })
+        }()
+    }
+
+    if (yiliaConfig.search) {
+        var search = function(){
+            require([yiliaConfig.rootUrl + 'js/search.js'], function(){
+                var inputArea = document.querySelector("#local-search-input");
+                var $HideWhenSearch = $("#toc, #tocButton, .post-list, #post-nav-button a:nth-child(2)");
+                var $resetButton = $("#search-form .fa-times");
+                var $resultArea = $("#local-search-result");
+
+                var getSearchFile = function(){
+                    var search_path = "search.xml";
+                    var path = yiliaConfig.rootUrl + search_path;
+                    searchFunc(path, 'local-search-input', 'local-search-result');
+                }
+
+                var getFileOnload = inputArea.getAttribute('searchonload');
+                if (yiliaConfig.search && getFileOnload === "true") {
+                    getSearchFile();
+                } else {
+                    inputArea.onfocus = function(){ getSearchFile() }
+                }
+
+                var HideTocArea = function(){
+                    $HideWhenSearch.css("visibility","hidden");
+                    $resetButton.show();
+                }
+                inputArea.oninput = function(){ HideTocArea() }
+                inputArea.onkeydown = function(){ if(event.keyCode==13) return false}
+
+                resetSearch = function(){
+                    $HideWhenSearch.css("visibility","initial");
+                    $resultArea.html("");
+                    document.querySelector("#search-form").reset();
+                    $resetButton.hide();
+                    $(".no-result").hide();
+                }
+
+                $resultArea.bind("DOMNodeRemoved DOMNodeInserted", function(e) {
+                    if (!$(e.target).text()) {
+                        $(".no-result").show(200);
+                    } else {
+                      $(".no-result").hide();
+                    }
+                })
+            })
+        }()
+    }
+
+    return {
+        init: function(){
+            resetTags();
+            bind();
+            Tips.init();
+        }
+    }
+});
